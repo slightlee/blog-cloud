e0da319a4aea976117548971f9175e512f3aabe1
demain_lee <limingwei@mentpeak.com>
2022-12-05 16:50:07 +0800

    :sparkles: 自定义负载均衡算法

--- blog-service/blog-content/src/main/java/com/demain/content/controller/ContentController.java
+++ blog-service/blog-content/src/main/java/com/demain/content/controller/ContentController.java
@@ -10,12 +10,17 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * <p>
@@ -29,28 +34,29 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/content")
 @RequiredArgsConstructor
 @Tag(name = "内容管理")
+@Slf4j
 public class ContentController {
 
 	private final ContentService contentService;
-
-//	private final RestTemplate restTemplates;
 
 	/**
 	 * 根据内容ID获取内容信息
 	 */
 	@GetMapping("/getContentInfo/{id}")
 	@Operation(summary = "根据内容ID获取内容信息")
-	@Parameters({
-			@Parameter(name = "id", description = "内容ID")
-	})
+	@Parameters({ @Parameter(name = "id", description = "内容ID") })
 	public Result<ContentInfoV> getContentInfo(@PathVariable("id") Long id) {
 
 		// 内容信息
 		Content content = contentService.getById(id);
 		// 用户信息
 		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<User> responseEntity = restTemplate.getForEntity("http://127.0.0.1:8001/user/getUserInfo/{id}", User.class, id);
-//		ResponseEntity<User> responseEntity = restTemplates.getForEntity("http://127.0.0.1:8001/user/getUserInfo/{id}", User.class, id);
+
+		List<String> urlList = Arrays.asList("http://127.0.0.1:8001", "http://127.0.0.1:8003");
+		int i = ThreadLocalRandom.current().nextInt(urlList.size());
+		String url = urlList.get(i);
+		log.info("请求url为:{}",url);
+		ResponseEntity<User> responseEntity = restTemplate.getForEntity(url + "/user/getUserInfo/{id}", User.class, id);
 		User user = responseEntity.getBody();
 		assert user != null;
 		ContentInfoV contentInfoV = ContentInfoV.builder().content(content).nickName(user.getNickName()).build();
