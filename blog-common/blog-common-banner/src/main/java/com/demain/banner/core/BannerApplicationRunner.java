d5651ce250eff0ce27118851330b5bd72b2051bb
demain_lee <lmm_work@163.com>
2023-01-06 17:22:56 +0800

    :sparkles: banner组件增加服务名以及端口

--- blog-common/blog-common-banner/src/main/java/com/demain/banner/core/BannerApplicationRunner.java
+++ blog-common/blog-common-banner/src/main/java/com/demain/banner/core/BannerApplicationRunner.java
@@ -2,8 +2,10 @@ package com.demain.banner.core;
 
 import cn.hutool.core.thread.ThreadUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,18 +18,24 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class BannerApplicationRunner implements ApplicationRunner {
 
+	@Value("${spring.application.name}")
+	private String applicationName;
+
+	@Value("${server.port}")
+	private String port;
+
 	@Override
 	public void run(ApplicationArguments args) {
 
 		ThreadUtil.execute(() -> {
 			ThreadUtil.sleep(1, TimeUnit.SECONDS); // 延迟 1 秒，保证输出到结尾
-			log.info(
-					"""
+			log.info("""
 
-							==========================================================
-							\t 服务启动成功 ！！！
-							\t 接口文档: \t{}\s
-							==========================================================""",
+					==========================================================
+					\t {} 服务启动成功 φ(゜▽゜*)♪
+					\t 占用端口: \t{}\s
+					\t 接口文档: \t{}\s
+					==========================================================""", applicationName, port,
 					"http://127.0.0.1:port/doc.html");
 		});
 	}
