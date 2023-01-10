4b185cab0cf7196024d1331136dcd6acba076c15
demain_lee <lmm_work@163.com>
2023-01-10 09:51:30 +0800

    :sparkles: 简单集成gateway

---
+++ blog-gateway/src/main/java/com/demain/gateway/GateWayApplication.java
@@ -0,0 +1,19 @@
+package com.demain.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 网关服务
+ *
+ * @author demain_lee
+ * @since 0.0.1
+ */
+@SpringBootApplication
+public class GateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GateWayApplication.class, args);
+	}
+
+}
